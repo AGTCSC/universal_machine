aa2cd307eba26c7c95acecad036dc235bada5ed3
AGTCSC <aceesar99@gmail.com>
2016-03-29 11:10:51 -0300

    Update turing.c

--- turing.c
+++ turing.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
 5
 Q0 # # D a0
@@ -11,7 +12,7 @@ typedef struct turing
 {
 	int i ,f;
 	char a, b, dir;
-};
+}turing;
 
 void gerarArq()
 {
@@ -21,7 +22,7 @@ void gerarArq()
 	   printf("Problemas na CRIACAO do arquivo\n");
 	   return;
 	}
-	if(fputs("0 @ @ d 0\n0 a A d 1\n0 B B d 3\n0 _ _ d 4\n1 a a d 1\n1 B B d 1\n2 a a e 2\n2 B B e 2\n2 A A d 0\n3 B B d 3\n3 _ _ e 4\n4 * * * *", arq)== EOF)
+	if(fputs("12\n0 @ @ d 0\n0 a A d 1\n0 B B d 3\n0 _ _ d 4\n1 a a d 1\n1 B B d 1\n2 a a e 2\n2 B B e 2\n2 A A d 0\n3 B B d 3\n3 _ _ e 4\n4 * * * *", arq)== EOF)
 	{
 		printf("ERRO NA CRIACAO\n");
 		return;
@@ -34,7 +35,7 @@ void gerarArq()
 	   printf("Problemas na CRIACAO do arquivo\n");
 	   return;
 	}
-	if(fputs(	"0 @ @ d 0\n0 a a d 0\n0 b b d 0\n0 # # d 0\n"
+	if(fputs(	"15\n0 @ @ d 0\n0 a a d 0\n0 b b d 0\n0 # # d 0\n"
 				"0 _ _ e 1\n1 # _ e 4\n1 b _ e 3\n1 a _ e 2\n"
 				"2 a a e 2\n2 b a e 3\n2 # a e 4\n3 b b e 3\n"
 				"3 a b e 2\n3 # b e 4\n4 * * * *", arq)== EOF)
@@ -50,7 +51,7 @@ void gerarArq()
 	   printf("Problemas na CRIACAO do arquivo\n");
 	   return;
 	}
-	if(fputs(	"0 @ @ d 0\n0 B B d 0\n0 1 A d 1\n0 _ _ e 3\n1 1 1 d 1\n1 B B d 1\n"
+	if(fputs(	"37\n0 @ @ d 0\n0 B B d 0\n0 1 A d 1\n0 _ _ e 3\n1 1 1 d 1\n1 B B d 1\n"
 				"1 _ B e 2\n2 1 1 e 2\n2 B B e 2\n2 A A d 0\n3 @ @ d 13\n3 B _ e 4\n"
 				"4 B B e 4\n\n4 A A d 5\n5 B C e 6\n5 _ _ e 12\n6 A 1 e 6\n6 C C e 6\n"
 				"6 @ @ d 7\n7 1 A d 8\n8 1 A d 9\n8 C C d 11\n9 C C d 9\n9 B B d 9\n"
@@ -62,7 +63,7 @@ void gerarArq()
 	}
 	fclose(arq);	
 }
-struct* turing maquina(int maq)
+struct turing* maquina(int maq)
 {
 	FILE* arq;
 	switch(maq)
@@ -78,30 +79,39 @@ struct* turing maquina(int maq)
 			arq = fopen("exemplo3.txt", "rt");
 		break;
 	}
+	char linha[15];
+	int qnt, i=0;
+	fscanf(arq, "%d", &qnt);
+	
+	printf("%d\n", qnt);
+	struct turing* t = (turing*) malloc (sizeof(turing)*qnt);
+	
+	while(fscanf(arq, "%d %c %c %c %d", &t[i].i, &t[i].a, &t[i].b, &t[i].dir, &t[i].f) != 0)
+	{				
+		printf("\n%d %c %c %c %d\n", t[i].i, t[i].a, t[i].b, t[i].dir, t[i].f);
+		i++;
+	}
 	
 	fclose(arq);
-	return arq;
+	return t;
 }
-void executa(int maq, char* entrada, int pos)
+void executa(int maq, char* entrada, int pos, turing* t)
 {
-	char linha[15];
-	int qnt;
-	struct* turing t;
-	FILE* arq;
-	arq = maquina(maq);
-	fscanf(arq, "%d", qnt);
-	turing = (turing*) malloc (sizeof(turing)*qnt);
+	int i=0;
 	
-	while(feof(arq))
+	while(t[i].a != '*')
 	{
-		sscanf(arq, "%d %c %c %c %d", t.i, t.a, t.b, t.dir, )
+		while(t[i].i == pos)
+		{
+			
+			i++;
+		}
 	}
-	linha = fgets(arq);
-	while()
 }
 int main()
 {
 	FILE* arq;
+	turing* t;
 	char in[100];
 	gerarArq();
 	int maq, pos=0;
@@ -110,7 +120,7 @@ int main()
 	{
 		printf("Qual maquina deseja simular? ");
 		scanf("%d", &maq);
-		arq = maquina(maq);
+		t = maquina(maq);
 		
 		printf("Informe a entrada[Limite de 20 caracteres]: ");
 		scanf("%s", in);
